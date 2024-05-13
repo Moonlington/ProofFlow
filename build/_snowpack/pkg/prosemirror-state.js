24a0f3cbcd9585c219ab07a3fe0242608a5c9562
Lars2203B <l.v.d.brom@student.tue.nl>
2024-05-13 11:16:01 +0200

    init

---
+++ build/_snowpack/pkg/prosemirror-state.js
@@ -0,0 +1,2 @@
+export { E as EditorState } from './common/index-b14b0919.js';
+import './common/index-b21c4b07.js';
---
+++ build/_snowpack/pkg/prosemirror-view.js
@@ -0,0 +1,3 @@
+export { E as EditorView } from './common/index-7a4ffd5c.js';
+import './common/index-b14b0919.js';
+import './common/index-b21c4b07.js';
