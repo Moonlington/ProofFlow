aa7117144f5e34c919866d586f1374bb18b60b95
josericho <joserichomr@gmail.com>
2024-05-27 11:15:52 +0200

    merge conflicts

--- src/ProofFlow/commands/insert-commands.ts
+++ src/ProofFlow/commands/insert-commands.ts
@@ -1,6 +1,6 @@
 import { NodeType } from "prosemirror-model";
-import { allowedToInsert, InsertionFunction } from "./helpers";
-import { Command, EditorState, Transaction } from "prosemirror-state";
+import {allowedToInsert, getContainingNode, InsertionFunction} from "./helpers";
+import {Command, EditorState, NodeSelection, Transaction} from "prosemirror-state";
 import { EditorView } from "prosemirror-view";
 import { GetPos } from "../codemirror/types.ts";
 import { CodeMirrorView } from "../codemirror";
@@ -75,46 +75,6 @@ export function getCodeInsertCommand(
     trans = insertionFunction(state, state.tr, codeblockNodeType);
     if (dispatch && trans) dispatch(trans);
 
-    return true;
-  };
-}
-
-export function getCollapsibleInsertCommand(): Command {
-  return (
-    state: EditorState,
-    dispatch?: (tr: Transaction) => void,
-    view?: EditorView,
-  ): boolean => {
-    if (!allowedToInsert(state)) return false;
-    let selection = state.selection;
-    let parent = getContainingNode(selection);
-    if (parent == undefined || parent.type.name != "doc") return false;
-    let oldNode = null;
-    let selectionType = getSelectionType(selection);
-    if (selectionType.isTextSelection) {
-      oldNode = selection.$from.node();
-    } else if (selectionType.isNodeSelection) {
-      oldNode = (selection as NodeSelection).node;
-    }
-    if (oldNode == null) return false;
-
-    let textNode: Node = collapsibleTitleNodeType.create(null, [
-      ProofFlowSchema.text("Collapsible: "),
-    ]);
-    let contentNode: Node = collapsibleContentType.create(
-      { visible: true },
-      [oldNode],
-    )
-    let collapsibleNode: Node = collapsibleNodeType.create({}, [textNode, contentNode]);
-    let trans: Transaction = state.tr;
-    if (selectionType.isTextSelection) {
-      let resolved = selection.$from;
-      console.log(resolved.start(), resolved.end());
-      trans.replaceWith(resolved.start() - 1, resolved.end(), collapsibleNode);
-    } else if (selectionType.isNodeSelection) {
-      trans.replaceSelectionWith(collapsibleNode);
-    }
-    if (dispatch && trans) dispatch(trans);
     return true;
   };
 }
