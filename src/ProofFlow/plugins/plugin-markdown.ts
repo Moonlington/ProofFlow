ea584b940d7c85689da8ad57cd035b76885de23a
Kaspar van Zelst <k.a.a.v.zelst@student.tue.nl>
2024-05-29 09:26:35 +0200

    Plugin testing

--- src/ProofFlow/plugins/plugin-markdown.ts
+++ src/ProofFlow/plugins/plugin-markdown.ts
@@ -2,11 +2,16 @@ import {EditorState, Plugin} from "prosemirror-state";
 import {EditorView} from "prosemirror-view";
 
 export const testPlugin = new Plugin({
+    
     view() {
         return {
             update(view: EditorView) {
-                console.log(view.state.doc.childCount);
+                let $from = view.state.selection.$from;
+                let depth = $from.depth
+                let node = $from.node(depth);
+                console.log(node.type.name);
             }
+            
         }
     }
 });
