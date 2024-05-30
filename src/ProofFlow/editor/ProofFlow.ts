170768b5f3b12630bc46c648c9d7529ede1336b8
KerelKarel <k.r.goddijn@student.tue.nl>
2024-05-30 23:14:42 +0200

    Track changes and better scroll.

--- src/ProofFlow/editor/ProofFlow.ts
+++ src/ProofFlow/editor/ProofFlow.ts
@@ -228,8 +228,6 @@ export class ProofFlow {
         }
       }
     }
-
-    this.minimap.updateHTML();
   }
 
   /**
--- src/ProofFlow/minimap.ts
+++ src/ProofFlow/minimap.ts
@@ -11,8 +11,10 @@ export class Minimap {
     private _minimapContentDiv: HTMLIFrameElement;
     private _bodyScale = 0;
     private _realScale = 0;
+    private _config = { attributes: true, childList: true, subtree: true };
+    private observer: MutationObserver;
 
-    constructor(html: string) {
+    constructor() {
         this._minimapDiv = document.createElement('div');
         this._minimapSizeDiv = document.createElement('div');
         this._minimapViewerDiv = document.createElement('div');
@@ -29,10 +31,28 @@ export class Minimap {
         this.updateHTML();
 
         this.getDimensions()
+
+        this.observer = new MutationObserver(this.callback);
+        this.start();
+    }
+
+    public start() {
         const editor = document.getElementById("editor");
+        this.observer.observe(editor!, this._config);
         editor!.addEventListener('scroll', () => this.trackScroll())
         editor!.addEventListener('resize', () => this.getDimensions())
     }
+
+    public stop() {
+        const editor = document.getElementById("editor");
+        this.observer.disconnect();
+        editor!.removeEventListener('scroll', this.trackScroll);
+        editor!.addEventListener('resize', () => this.getDimensions);
+    }
+
+    private callback = (mutationList: MutationRecord[], observer: MutationObserver) => {
+        this.updateHTML();
+    };
 
     public updateHTML() {
         const editor = document.getElementById("editor");
@@ -51,8 +71,8 @@ export class Minimap {
             iframeDoc.head.appendChild(newStyleTag);
         });
         
-        this.trackScroll();
         this.getDimensions();
+        this.trackScroll();
     }
 
     private getDimensions(){
@@ -64,6 +84,7 @@ export class Minimap {
         let winRatio = window.innerHeight / window.innerWidth;
     
         this._minimapDiv.style.width = '15%';
+        this._minimapDiv.style.height = '90%';
         
 
         this._realScale = this._minimapDiv.clientWidth / bodyWidth;
@@ -72,9 +93,9 @@ export class Minimap {
         this._minimapSizeDiv.style.paddingTop = `${bodyRatio * 100}%`
         this._minimapViewerDiv.style.paddingTop = `${winRatio * 100}%`;
 
-        console.log(editor!.clientHeight, this._realScale);
         this._minimapViewerDiv.style.height = `${(editor!.clientHeight * this._bodyScale)}px`
     
+        console.log(this._realScale);
         this._minimapContentDiv.style.transform = `scale(${this._realScale})`;
         this._minimapContentDiv.style.width = `${(100 / this._realScale)}%`
         this._minimapContentDiv.style.height = `${(100 / this._realScale)}%`
@@ -83,7 +104,7 @@ export class Minimap {
     private trackScroll(){
         // console.log(this);
         const editor = document.getElementById("editor");
-        this._minimapViewerDiv.style.transform = `translateY(${editor!.scrollTop * this._realScale}px)`
-        // this._minimapContentDiv.style.transform = `translateY(${-editor!.scrollTop * this._realScale}px)`
+        this._minimapViewerDiv.style.transform = `translateY(${editor!.scrollTop * this._realScale * 0.5}px)`
+        this._minimapContentDiv.style.transform = `translateY(${-editor!.scrollTop * this._realScale * 0.5}px) scale(${this._realScale})`;
     }
 }
