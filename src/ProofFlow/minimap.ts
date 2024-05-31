3abaa14593043559e6b5a76c21ee24c61e192f15
Karel Goddijn <k.r.goddijn@student.tue.nl>
2024-05-31 10:02:21 +0200

    Improved speed drastically.
    Throttling was not implemented correctly.

--- src/ProofFlow/minimap.ts
+++ src/ProofFlow/minimap.ts
@@ -14,7 +14,9 @@ export class Minimap {
     private _config = { attributes: true, childList: true, subtree: true };
     private observer: MutationObserver;
 
-    private timeoutId = 0;
+    private timeoutIdHTML = 0;
+    private timeoutIdScroll = 0;
+    private timeoutIdResize = 0;
     private debounceDelay = 300; // Adjust delay as needed
 
     constructor() {
@@ -31,7 +33,6 @@ export class Minimap {
         this._minimapDiv.append(this._minimapSizeDiv, this._minimapViewerDiv, this._minimapContentDiv);
         document.body.appendChild(this._minimapDiv);
 
-        this.updateHTML.bind(this);
         this.updateHTML();
 
         this.getDimensions()
@@ -43,25 +44,35 @@ export class Minimap {
     public start() {
         const editor = document.getElementById("editor");
         this.observer.observe(editor!, this._config);
-        editor!.addEventListener('scroll', () => this.trackScroll())
-        editor!.addEventListener('resize', () => this.getDimensions())
+        editor!.addEventListener('scroll', () => {
+            if (this.timeoutIdScroll != 0) return;
+            this.timeoutIdScroll = setTimeout(this.trackScroll.bind(this), this.debounceDelay / 3);
+        })
+        editor!.addEventListener('resize', () => {
+            if (this.timeoutIdResize != 0) return;
+            this.timeoutIdResize = setTimeout(this.getDimensions.bind(this), this.debounceDelay / 3);
+        })
     }
 
     public stop() {
+        this.timeoutIdHTML = 0;
+        this.timeoutIdScroll = 0;
+        this.timeoutIdResize = 0;
         const editor = document.getElementById("editor");
         this.observer.disconnect();
-        editor!.removeEventListener('scroll', this.trackScroll);
+        editor!.removeEventListener('scroll', () => this.trackScroll);
         editor!.addEventListener('resize', () => this.getDimensions);
     }
 
     private callback = (mutationList: MutationRecord[], observer: MutationObserver) => {
-        if (this.timeoutId != 0) return;
-        setTimeout(this.updateHTML.bind(this), this.debounceDelay);
+        if (this.timeoutIdHTML != 0) return;
+        console.log("Timeout set");
+        this.timeoutIdHTML = setTimeout(this.updateHTML.bind(this), this.debounceDelay);
     };
 
     public updateHTML() {
         console.log("updateHTML");
-        this.timeoutId = 0;
+        this.timeoutIdHTML = 0;
 
         const editor = document.getElementById("editor");
         const html = editor!.innerHTML;
@@ -84,6 +95,7 @@ export class Minimap {
     }
 
     private getDimensions(){
+        this.timeoutIdResize = 0;
         const editor = document.getElementById("editor");
 
         let bodyWidth = editor!.clientWidth;
@@ -103,16 +115,28 @@ export class Minimap {
 
         this._minimapViewerDiv.style.height = `${(editor!.clientHeight * this._bodyScale)}px`
     
-        console.log(this._realScale);
         this._minimapContentDiv.style.transform = `scale(${this._realScale})`;
         this._minimapContentDiv.style.width = `${(100 / this._realScale)}%`
-        this._minimapContentDiv.style.height = `${(100 / this._realScale)}%`
+        this._minimapContentDiv.style.height = `${(bodyHeight / this._realScale)}%`
     }
     
     private trackScroll(){
+        this.timeoutIdScroll = 0;
+
         // console.log(this);
         const editor = document.getElementById("editor");
-        this._minimapViewerDiv.style.transform = `translateY(${editor!.scrollTop * this._realScale * 0.5}px)`
         this._minimapContentDiv.style.transform = `translateY(${-editor!.scrollTop * this._realScale * 0.5}px) scale(${this._realScale})`;
+        this._minimapViewerDiv.style.transform = `translateY(${editor!.scrollTop * this._realScale * 0.5}px)`
+        // console.log(editor!.scrollTop, editor!.scrollTop + editor!.clientHeight, editor!.clientHeight * this._bodyScale);
+        // console.log(editor!.scrollTop, editor!.clientHeight)
+
+        /*const startViewerOffset = editor!.scrollTop * this._realScale;
+        if ((editor!.scrollTop - editor!.clientHeight) > 0) {
+            const ratio = 1 - ((editor!.scrollHeight - editor!.scrollTop - editor!.clientHeight) / (editor!.scrollHeight - 2 * editor!.clientHeight));
+            this._minimapViewerDiv.style.transform = `translateY(${startViewerOffset + ratio * this._realScale * (editor!.scrollHeight - editor!.scrollTop - editor!.clientWidth) / 2}px)`
+        } else {
+            this._minimapViewerDiv.style.transform = `translateY(${startViewerOffset}px)`
+        }*/
+        // this._minimapContentDiv.style.transform = `translateY(${-editor!.scrollTop * this._realScale * 0.5}px) scale(${this._realScale})`;
     }
 }
