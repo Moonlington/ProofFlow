a5614c4e02a7b64c84ea61139a1fb9ef5a3b58ab
Moonlington <Moonlington@users.noreply.github.com>
2024-06-26 16:51:05 +0200

    FIX: invalid defined document not parsing to output well

--- src/ProofFlow/parser/parsers.ts
+++ src/ProofFlow/parser/parsers.ts
@@ -1,3 +1,4 @@
+import { CoqMDOutput, CoqOutput, LeanOutput, PureLeanOutput } from "./outputconfigs";
 import { SimpleParser } from "./parser";
 
 export { CoqParser, CoqMDParser, LeanParser, PureLeanParser };
@@ -10,25 +11,25 @@ const CoqParser = new SimpleParser({
     /\n\(\*<\/hint>\*\)\n/,
   ],
   input: [/\n\(\*<input-area>\*\)\n/, /\n\(\*<\/input-area>\*\)\n/],
-});
+}, CoqOutput);
 
 const CoqMDParser = new SimpleParser({
   code: [/\n```coq\n/, /\n```\n/],
   math: [/\$\$/, /\$\$/],
   collapsible: [/<hint(?: title="(.*?)")?>/, /<\/hint>/],
   input: [/<input-area>/, /<\/input-area>/],
-});
+}, CoqMDOutput);
 
 const LeanParser = new SimpleParser({
   code: [/\n```lean\n/, /\n```\n/],
   math: [/\n:::math\n/, /\n:::\n/],
   collapsible: [/\n:::collapsible\n(?:#(.*?)\n)?/, /\n:::\n/],
   input: [/\n:::input\n\.\n/, /\n:::\n/],
-});
+}, LeanOutput);
 
 const PureLeanParser = new SimpleParser({
   text: [/\n\/-\n/, /\n-\/\n/],
   math: [/\n\/-\$\$\n/, /\n\$\$-\/\n/],
   collapsible: [/\n-- <hint(?: title="(.*?)")?>\n?/, /\n-- <\/hint>\n/],
   input: [/\n-- <input-area>\n/, /\n-- <\/input-area>\n/],
-});
+}, PureLeanOutput);
