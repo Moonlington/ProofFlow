37c8a29b4be4b0bc0138d00529a58c5c7d652c38
Lars2203B <l.v.d.brom@student.tue.nl>
2024-07-03 10:18:12 +0200

    replace 1 function

--- src/ProofFlow/editor/ButtonBar.ts
+++ src/ProofFlow/editor/ButtonBar.ts
@@ -7,6 +7,7 @@ import {
 import {
   InsertionPlace,
   getContainingNode,
+  requestConfirm,
   toggleLineNumbers,
 } from "../commands/helpers.ts";
 import { EditorView } from "prosemirror-view";
@@ -216,7 +217,7 @@ export class ButtonBar {
         symbol: "&#x21bb;",
         cmd: async () => {
           if (
-            await proofFlow.requestConfirm(
+            await requestConfirm(
               "Are you sure you want to clear the file?",
             )
           ) {
